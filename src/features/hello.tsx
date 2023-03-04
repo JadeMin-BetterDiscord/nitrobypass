410797caf9456e06b112c290e4c0075db6e8c9cc
JadeMin <alt0000@klartnet.ml>
2023-03-04 23:24:25 +0900

    업데이트

--- src/features/hello.tsx
+++ src/features/hello.tsx
@@ -1,5 +1,5 @@
-import type { APIMessage } from 'discord-api-types/v10';
-import type { ToCamel } from "../../@types/extensions.js";
+/*import type { APIMessage } from 'discord-api-types/v10';
+import type { ToCamel } from "../../@types/extensions.js";*/
 import type { FeatureModule } from "../features.js";
 
 import { prompt } from "./.modules/prompt.js";
@@ -10,14 +10,14 @@ const { React, Webpack, Patcher, UI } = BdApi;
 const module: FeatureModule = {
 	async start() {
 		const result = await prompt("테스트", "테스트입니다.");
-		UI.showNotice(`당신이 쳐입력한 값은 "${result}"입니다.`, {
+		UI.showToast(`입력된 값은 "${result}"입니다.`, {
+			type: 'info',
 			timeout: 3000,
 		});
 
 		/*UI.showConfirmationModal("안녕", (
 			<div>
-				<p>당신이 쳐입력한 값은 "{result}"입니다.</p>
-				<p>이 값이 맞습니까?</p>
+				<p>입력된 값은 "{result}"입니다.</p>
 			</div>
 		));*/
 	},
